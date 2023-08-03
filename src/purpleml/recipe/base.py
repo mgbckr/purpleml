8ff0c88057570562a6bb3eebd91fa03fbd328685
mgbckr <1241516+mgbckr@users.noreply.github.com>
2023-08-03 22:52:45 +0200

    Update

--- src/purpleml/recipe/base.py
+++ src/purpleml/recipe/base.py
@@ -274,27 +274,3 @@ class Recipe():
             for option_name, _ in step_data["options"].items():
                 string += f"      - {option_name}\n"
         return string
-
-
-# TEST
-recipe = Recipe()
-
-string = "BLUBB"
-
-@recipe.step(step_type="fixed")
-def first(input_str):
-    return input_str + "_first"
-
-@recipe.step("pp", step_type="branch")
-def test1(input_str):
-    return input_str + "_test1"
-
-@recipe.step("pp")
-def test2(input_str):
-    return input_str + "_test2"
-
-print()
-print(str(recipe))
-
-recipe.execute("blubb", last_step="first")
-# recipe.execute_latest("blubb")
